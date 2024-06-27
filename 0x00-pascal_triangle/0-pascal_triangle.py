d1ed1461864144e0ef79f9d9cd96331c8429e17d
AmgaadShokrallah <amgaad@example.com>
2024-06-27 09:24:34 +0000

    commit

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of integers
+    returns empty list if n <= 0
+    """
+    t = []
+    if n <= 0:
+        return t
+    t = [[1]]
+    for i in range(1, n):
+        tmp = [1]
+        for j in range(len(t[i - 1]) - 1):
+            curr = t[i - 1]
+            tmp.append(t[i - 1][j] + t[i - 1][j + 1])
+        tmp.append(1)
+        t.append(tmp)
+    return t
